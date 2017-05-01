5034071b3096797d3656524d011c51bb048f8e58
Stefan Mermas <stefan_mermans@hotmail.com>
2017-05-01 16:16:06 +0200

    Refactored openGL loop to be more OO

--- ProftaakPeriode4/ProftaakPeriode4/View.cpp
+++ ProftaakPeriode4/ProftaakPeriode4/View.cpp
@@ -2,10 +2,115 @@
 
 View::View(Model * model)
 {
+	_screenWidth = 1280;
+	_screenHeight = 720;
+
+	_camNear = 0.01f;
+	_camFar = 50.0f;
+
 	_modelPtr = model;
 
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-	glutInitWindowSize(width, height);
+	glutInitWindowSize(_screenWidth, _screenHeight);
 
 	glutCreateWindow("Cube_Runner");
+}
+
+View::View()
+{
+	_screenWidth = 1280;
+	_screenHeight = 720;
+
+	_camNear = 0.01f;
+	_camFar = 50.0f;
+}
+
+void drawCube()
+{
+	glColor3f(1, 0, 0);
+	glBegin(GL_QUADS);
+	//front
+	glVertex3f(-0.5, -0.5, -0.5);
+	glVertex3f(0.5, -0.5, -0.5);
+	glVertex3f(0.5, 0.5, -0.5);
+	glVertex3f(-0.5, 0.5, -0.5);
+
+	//left
+	glColor3f(0, 1, 0);
+	glVertex3f(-0.5, -0.5, 0.5);
+	glVertex3f(-0.5, -0.5, -0.5);
+	glVertex3f(-0.5, 0.5, -0.5);
+	glVertex3f(-0.5, 0.5, 0.5);
+
+	//back
+	glColor3f(1, 0, 0);
+	glVertex3f(-0.5, -0.5, 0.5);
+	glVertex3f(0.5, -0.5, 0.5);
+	glVertex3f(0.5, 0.5, 0.5);
+	glVertex3f(-0.5, 0.5, 0.5);
+
+	//right
+	glColor3f(0, 1, 0);
+	glVertex3f(0.5, -0.5, 0.5);
+	glVertex3f(0.5, -0.5, -0.5);
+	glVertex3f(0.5, 0.5, -0.5);
+	glVertex3f(0.5, 0.5, 0.5);
+
+	//top
+	glColor3f(0, 0, 1);
+	glVertex3f(-0.5, 0.5, -0.5);
+	glVertex3f(0.5, 0.5, -0.5);
+	glVertex3f(0.5, 0.5, 0.5);
+	glVertex3f(-0.5, 0.5, 0.5);
+
+	//bottom
+	glColor3f(0, 0, 1);
+	glVertex3f(-0.5, -0.5, -0.5);
+	glVertex3f(0.5, -0.5, -0.5);
+	glVertex3f(0.5, -0.5, 0.5);
+	glVertex3f(-0.5, -0.5, 0.5);
+
+	glEnd();
+}
+
+void View::update()
+{
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	gluPerspective(90.0f, _screenWidth / (float)_screenHeight, _camNear, _camFar);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	gluLookAt(0, 5, 5,
+		0, 0, 0,
+		0, 1, 0);
+
+	//Models are drawn here..
+//#ifdef DEBUG
+	glPushMatrix();
+	//glTranslatef(posX, posY, posZ);
+	//glRotatef(rotation, 0, 1, 0);
+	drawCube();
+	glPopMatrix();
+//#endif // DEBUG
+
+
+
+	glEnable(GL_DEPTH_TEST);
+
+	glutSwapBuffers();
+}
+
+void View::reshape(int w, int h)
+{
+	_screenWidth = w;
+	_screenHeight = h;
+	glViewport(0, 0, w, h);
 }
--- ProftaakPeriode4/ProftaakPeriode4/View.h
+++ ProftaakPeriode4/ProftaakPeriode4/View.h
@@ -1,11 +1,19 @@
 #pragma once
 #include <GL\freeglut.h>
 #include "Model.h"
-#include "Window.h"
 
 class View {
-	public:
-		View(Model * model);
-	private:
-		Model * _modelPtr;
+public:
+	View(Model * model);
+	View();
+
+	void update();
+	void reshape(int w, int h);
+private:
+	int _screenWidth;
+	int _screenHeight;
+	float _camNear;
+	float _camFar;
+	
+	Model * _modelPtr;
 };
